b4b971731bd6ec97e6a6835f580387d64de4c31e
Lebogang <phoshokolebogang@outlook.com>
2024-08-16 09:41:58 +0200

    Update: remove keys

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from spotipy.oauth2 import SpotifyClientCredentials
 from typing import List, Tuple, Dict
 import spotipy, os, time, requests
 
-CLIENT_ID = "b15dccc7e05b4f90bf350da6ff1e0be8"
-CLIENT_SECRET = "67313c18a40c474d90b25b8f89ea1b16"
+CLIENT_ID = os.environ.get("CLIENT_ID")
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
 
 auth_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 sp = spotipy.Spotify(auth_manager=auth_manager)
